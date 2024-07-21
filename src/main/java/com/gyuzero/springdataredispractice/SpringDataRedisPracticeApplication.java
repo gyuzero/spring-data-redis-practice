ebf671a52481117110db97ef39c64696546789d2
gyuzero <gyuzero0103@gmail.com>
2024-07-21 18:44:15 +0900

    initial commit

---
+++ src/main/java/com/gyuzero/springdataredispractice/SpringDataRedisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.gyuzero.springdataredispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisPracticeApplication.class, args);
+    }
+
+}
